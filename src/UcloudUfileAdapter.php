5843d832285cab324c2bc7bab25d5a30d1fa5302
xujif <i@xujif.com>
2015-11-11 14:19:56 +0800

    fix bugs

--- src/UcloudUfileAdapter.php
+++ src/UcloudUfileAdapter.php
@@ -3,21 +3,28 @@ namespace Xujif\UcloudUfileStorage;
 
 use League\Flysystem\Adapter\AbstractAdapter;
 use League\Flysystem\Adapter\Polyfill\NotSupportingVisibilityTrait;
-use League\Flysystem\Adapter\Polyfill\StreamedReadingTrait;
-use League\Flysystem\Adapter\Polyfill\StreamedWritingTrait;
+use League\Flysystem\Adapter\Polyfill\StreamedCopyTrait;
+use League\Flysystem\Adapter\Polyfill\StreamedTrait;
 use League\Flysystem\Config;
+use LogicException;
 use Xujif\UcloudUfileSdk\UfileSdk;
+
 class UcloudUfileAdapter extends AbstractAdapter {
 
-	use NotSupportingVisibilityTrait, StreamedWritingTrait, StreamedReadingTrait;
+	use NotSupportingVisibilityTrait, StreamedTrait, StreamedCopyTrait;
 	protected $ufileSdk;
 
+	public function __construct($bucket, $public_key, $secret_key, $suffix = '.ufile.ucloud.cn', $pathPrefix = '', $https = false) {
+		$this->ufileSdk = new UfileSdk($bucket, $public_key, $secret_key, $suffix, $https);
 
-
-
-	public function __construct($bucket, $public_key, $secret_key, $suffix = '.ufile.ucloud.cn',$pathPrefix='', $https = false) {
-		$this->ufileSdk = new UfileSdk($bucket,$public_key,$secret_key,$suffix,$https);
 		$this->setPathPrefix($pathPrefix);
+	}
+
+	public function read($path) {
+		$path = $this->applyPathPrefix($path);
+		$data = [];
+		$data['contents'] = $this->ufileSdk->get($path);
+		return $data;
 	}
 
 	/**
@@ -34,7 +41,8 @@ class UcloudUfileAdapter extends AbstractAdapter {
 		$params = $config->get('params', null);
 		$mime = $config->get('mime', 'application/octet-stream');
 		$checkCrc = $config->get('checkCrc', false);
-		$list($ret,$code) = $this->ufileSdk->put($path,$contents,['Content-Type'=>$mime]);
+		list($ret, $code) = $this->ufileSdk->put($path, $contents, ['Content-Type' => $mime]);
+		return $ret;
 	}
 
 	/**
@@ -51,7 +59,7 @@ class UcloudUfileAdapter extends AbstractAdapter {
 		$params = $config->get('params', null);
 		$mime = $config->get('mime', 'application/octet-stream');
 		$checkCrc = $config->get('checkCrc', false);
-		$list($ret,$code) = $this->ufileSdk->put($path,$resource,['Content-Type'=>$mime]);
+		list($ret, $code) = $this->ufileSdk->put($path, $resource, ['Content-Type' => $mime]);
 	}
 
 	/**
@@ -64,7 +72,7 @@ class UcloudUfileAdapter extends AbstractAdapter {
 	 * @return array|false false on failure file meta data on success
 	 */
 	public function update($path, $contents, Config $config) {
-		return $this->write($path,$contents,$config);
+		return $this->write($path, $contents, $config);
 	}
 
 	/**
@@ -77,7 +85,7 @@ class UcloudUfileAdapter extends AbstractAdapter {
 	 * @return array|false false on failure file meta data on success
 	 */
 	public function updateStream($path, $resource, Config $config) {
-		return $this->writeStream($path,$resource,$config);
+		return $this->writeStream($path, $resource, $config);
 	}
 
 	/**
@@ -89,18 +97,6 @@ class UcloudUfileAdapter extends AbstractAdapter {
 	 * @return bool
 	 */
 	public function rename($path, $newpath) {
-		throw new \Exception('not supprot');
-	}
-
-	/**
-	 * Copy a file.
-	 *
-	 * @param string $path
-	 * @param string $newpath
-	 *
-	 * @return bool
-	 */
-	public function copy($path, $newpath) {
 		throw new \Exception('not supprot');
 	}
 
@@ -124,7 +120,7 @@ class UcloudUfileAdapter extends AbstractAdapter {
 	 * @return bool
 	 */
 	public function deleteDir($dirname) {
-		throw new \Exception('not supprot');
+		throw new LogicException(get_class($this) . ' does not support ' . __FUNCTION__);
 	}
 
 	/**
@@ -140,14 +136,78 @@ class UcloudUfileAdapter extends AbstractAdapter {
 	}
 
 	/**
-	 * Set the visibility for a file.
+	 * Check whether a file exists.
 	 *
 	 * @param string $path
-	 * @param string $visibility
 	 *
-	 * @return array|false file meta data
+	 * @return array|bool|null
 	 */
-	public function setVisibility($path, $visibility) {
-		return [];
+	public function has($path) {
+		$path = $this->applyPathPrefix($path);
+		return $this->ufileSdk->exists($path);
 	}
+
+	/**
+	 * List contents of a directory.
+	 *
+	 * @param string $directory
+	 * @param bool   $recursive
+	 *
+	 * @return array
+	 */
+	public function listContents($directory = '', $recursive = false) {
+		throw new LogicException(get_class($this) . ' does not support ' . __FUNCTION__);
+	}
+
+	/**
+	 * Get all the meta data of a file or directory.
+	 *
+	 * @param string $path
+	 *
+	 * @return array|false
+	 */
+	public function getMetadata($path) {
+		$path = $this->applyPathPrefix($path);
+		$this->ufileSdk->meta($path);
+	}
+
+	/**
+	 * Get all the meta data of a file or directory.
+	 *
+	 * @param string $path
+	 *
+	 * @return array|false
+	 */
+	public function getSize($path) {
+		$path = $this->applyPathPrefix($path);
+		$size = $this->ufileSdk->size($path);
+		return array('size' => $size);
+	}
+
+	/**
+	 * Get the mimetype of a file.
+	 *
+	 * @param string $path
+	 *
+	 * @return array|false
+	 */
+	public function getMimetype($path) {
+		$path = $this->applyPathPrefix($path);
+		$mimeType = $this->ufileSdk->mime($path);
+		return array('mimetype' => $mimeType);
+	}
+
+	/**
+	 * Get the timestamp of a file.
+	 *
+	 * @param string $path
+	 *
+	 * @return array|false
+	 */
+	public function getTimestamp($path) {
+		$meta = $this->getMetadata($path);
+		$timestamp = strtotime($meta['Last-Modified']);
+		return array('timestamp' => $timestamp);
+	}
+
 }
